4f1dfdb3f2ed0fe4bb8d722cd9e61b76786cbd2a
lillann <annl@chalmers.se>
2017-12-07 09:14:44 +0100

    writes tptp to file. (uses the last row as the conjecture)

--- ttg/TTG.hs
+++ ttg/TTG.hs
@@ -10,9 +10,12 @@ import TTG2TT
 
 main = do
   pgf <- readPGF "Lang.pgf"
-  interact (interpret pgf)
-
-interpret :: PGF -> String -> String
+  s <- getContents 
+  let (s1,s2) = (interpret pgf s)
+  putStrLn s1
+  TT.writeTPTP "q.p" (init s2) (last s2)
+   
+interpret :: PGF -> String -> (String,[TT.FO])
 interpret pgf s =
   let
     phrs   = lines s
@@ -20,11 +23,11 @@ interpret pgf s =
     conts  = map (iPhrs . map fg) (sequence treess) ---- should not be expanded this way
     forms  = map prContext conts
     fols   = [[(x,TT.App x [] TT.-: ttg2tt p) | (x,p) <- co] | co <- conts]
-    tptps  = [[(x,TT.prTPTP p) | (x,p) <- co] | co <- fols]
-  in unlines $
+    tptps  = [[(x,p) | (x,p) <- co] | co <- fols]    
+  in (unlines $
          forms 
-      ++ map (show . snd) (concat fols)
-      ++ map snd (concat tptps)
+      ++ map (show . snd) (concat fols),
+       map snd (concat tptps))
 
 
 
